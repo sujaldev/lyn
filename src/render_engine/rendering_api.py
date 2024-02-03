e5c2d283e7e024825a7141da488b0a9fd565016f
Sujal Singh <email.sujalsingh@gmail.com>
2024-02-03 10:01:15 +0530

    temporary fix for all animations using initial state
    animations should be instantiated with the current state of the object rather than what it was initially declared with

--- src/render_engine/rendering_api.py
+++ src/render_engine/rendering_api.py
@@ -30,6 +30,10 @@ class LynRenderingApi:
         if not self.animation_queue:
             return
 
+        current_animation = self.animation_queue[-1]
+        if callable(current_animation):
+            self.animation_queue[-1] = self.animation_queue[-1]()
+
         animation_complete, completed_obj = self.animation_queue[-1].play(canvas, time)
         if animation_complete:
             self.animation_queue.pop()
@@ -37,19 +41,19 @@ class LynRenderingApi:
                 self.animated_objects.append(completed_obj)
 
     def draw(self, primitive):
-        self.animation_queue.append(animations.Draw(primitive))
+        self.animation_queue.append(lambda: animations.Draw(primitive))
 
     def write_line(self, primitive, duration=2):
-        self.animation_queue.append(animations.WriteLine(primitive, duration))
+        self.animation_queue.append(lambda: animations.WriteLine(primitive, duration))
 
     def fade_in(self, primitive, duration=1):
-        self.animation_queue.append(animations.FadeIn(primitive, duration))
+        self.animation_queue.append(lambda: animations.FadeIn(primitive, duration))
 
     def fade_out(self, primitive, duration=1):
-        self.animation_queue.append(animations.FadeOut(primitive, duration))
+        self.animation_queue.append(lambda: animations.FadeOut(primitive, duration))
 
     def delay(self, duration=1):
-        self.animation_queue.append(animations.Delay(duration))
+        self.animation_queue.append(lambda: animations.Delay(duration))
 
     def move(self, primitive, x, y, duration=2):
-        self.animation_queue.append(animations.Move(primitive, x, y, duration))
+        self.animation_queue.append(lambda: animations.Move(primitive, x, y, duration))
